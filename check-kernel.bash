420f4bdd7444f8e9fc72a24cec65cf4eb62337ca
YuviPanda <yuvipanda@gmail.com>
2016-10-09 11:09:33 +0200

    Use spaces consistently for check-kernel script

--- check-kernel.bash
+++ check-kernel.bash
@@ -3,20 +3,20 @@
 # Partially stolen from https://github.com/docker/docker/blob/master/contrib/check-config.sh
 
 possibleConfigs=(
-	  '/proc/config.gz'
-	  "/boot/config-$(uname -r)"
-	  "/usr/src/linux-$(uname -r)/.config"
-	  '/usr/src/linux/.config'
+    '/proc/config.gz'
+    "/boot/config-$(uname -r)"
+    "/usr/src/linux-$(uname -r)/.config"
+    '/usr/src/linux/.config'
 )
 
 for tryConfig in "${possibleConfigs[@]}"; do
-		if [ -e "$tryConfig" ]; then
-			  CONFIG="$tryConfig"
-			  break
-		fi
+    if [ -e "$tryConfig" ]; then
+        CONFIG="$tryConfig"
+        break
+    fi
 done
 if [ ! -e "$CONFIG" ]; then
-		echo "error: cannot find kernel config"
+    echo "error: cannot find kernel config"
     echo "please file an issue at https://github.com/jupyterhub/systemdspawner to help us fix this!"
     exit -1
 fi
