d373ede99080888ffcb54641c4afb38cfa25c844
YuviPanda <yuvipanda@gmail.com>
2016-10-09 19:27:24 +0200

    Slightly better description in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='jupyterhub-systemdspawner',
     version='0.1',
-    description='Spawner using systemd for resource isolation',
+    description='JupyterHub Spawner using systemd for resource isolation',
     url='https://github.com/jupyterhub/systemdspawner',
     author='Yuvi Panda',
     author_email='yuvipanda@gmail.com',
