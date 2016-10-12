c151c18c2d9bae953608ce448367c3ec2caccfee
YuviPanda <yuvipanda@gmail.com>
2016-10-11 20:27:42 -0700

    Bump version to v0.9.5 and set a long_description

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name='jupyterhub-systemdspawner',
-    version='0.1',
+    version='0.9.5',
     description='JupyterHub Spawner using systemd for resource isolation',
+    long_description='See https://github.com/jupyterhub/systemdspawner for more info',
     url='https://github.com/jupyterhub/systemdspawner',
     author='Yuvi Panda',
     author_email='yuvipanda@gmail.com',
     license='3 Clause BSD',
     packages=['systemdspawner'],
-    install_requires=['jupyterhub']
+    install_requires=['jupyterhub'],
 )
