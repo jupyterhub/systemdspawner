d0b927240410586d35f00506e853f4a41dab6f47
YuviPanda <yuvipanda@gmail.com>
2016-10-30 19:21:13 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jupyterhub-systemdspawner',
-    version='0.9.7',
+    version='0.9.8',
     description='JupyterHub Spawner using systemd for resource isolation',
     long_description='See https://github.com/jupyterhub/systemdspawner for more info',
     url='https://github.com/jupyterhub/systemdspawner',
