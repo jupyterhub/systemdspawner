dfc045f43343baf1aba7fcaf9b20f028e264675f
Min RK <benjaminrk@gmail.com>
2020-12-07 13:00:45 +0100

    setup.py tweaks
    - use readme for long description
    - specify 'systemd' entrypoint rather than package name, following standard practices
    - bump version to next.dev

--- setup.py
+++ setup.py
@@ -1,22 +1,27 @@
 from setuptools import setup
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
-    name='jupyterhub-systemdspawner',
-    version='0.15.0',
-    description='JupyterHub Spawner using systemd for resource isolation',
-    long_description='See https://github.com/jupyterhub/systemdspawner for more info',
+    name="jupyterhub-systemdspawner",
+    version="0.16.0.dev0",
+    description="JupyterHub Spawner using systemd for resource isolation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/jupyterhub/systemdspawner',
     author='Yuvi Panda',
     author_email='yuvipanda@gmail.com',
     license='3 Clause BSD',
     packages=['systemdspawner'],
     entry_points={
-        'jupyterhub.spawners': [
-            'systemdspawner = systemdspawner:SystemdSpawner',
+        "jupyterhub.spawners": [
+            "systemd = systemdspawner:SystemdSpawner",
+            "systemdspawner = systemdspawner:SystemdSpawner",
         ],
     },
     install_requires=[
-        'jupyterhub>=0.9',
-        'tornado>=5.0'
+        "jupyterhub>=0.9",
+        "tornado>=5.0",
     ],
 )
