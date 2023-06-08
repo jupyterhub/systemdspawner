67087e834740c8f39070f4f2818ca0b227611257
Erik Sundell <erik.i.sundell@gmail.com>
2023-06-08 08:51:49 +0200

    Use warnings.warn instead of self.log.warning to help avoid duplications

--- systemdspawner/systemdspawner.py
+++ systemdspawner/systemdspawner.py
@@ -2,6 +2,7 @@ import asyncio
 import os
 import pwd
 import sys
+import warnings
 
 from jupyterhub.spawner import Spawner
 from jupyterhub.utils import random_port
@@ -157,16 +158,14 @@ class SystemdSpawner(Spawner):
 
         systemd_version = systemd.get_systemd_version()
         if systemd_version is None:
-            self.log.warning(
-                "Failed to parse systemd version from 'systemctl --version'"
-            )
+            warnings.warn("Failed to parse systemd version from 'systemctl --version'")
         elif systemd_version < SYSTEMD_REQUIRED_VERSION:
             self.log.critical(
                 f"systemd version {SYSTEMD_REQUIRED_VERSION} or higher is required, version {systemd_version} is used"
             )
             sys.exit(1)
         elif systemd_version < SYSTEMD_LOWEST_RECOMMENDED_VERSION:
-            self.log.warning(
+            warnings.warn(
                 f"systemd version {SYSTEMD_LOWEST_RECOMMENDED_VERSION} or higher is recommended, version {systemd_version} is used"
             )
 
